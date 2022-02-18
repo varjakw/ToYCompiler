61726fb6ce73c2b18e84d0e2adc95f7042daa12e
varjakw <varjakwolfe2@protonmail.com>
2022-02-18 20:20:33 +0000

    code to be lexer'd

---
+++ Test.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class Test {
+    public static void main(String args[]) {
+        int b;
+        int a = 12;
+        if (a == 12) {
+            System.out.print("yeehaw");
+        }
+
+    }
+}
